3acf470aa6219fb6a2aa4b3332a1d412d1f87f88
Reyon <chphungphat@gmail.com>
2023-04-14 01:10:37 +0700

    my arraylist

--- sources/lesson-10/src/MyListTest/MyList.java
+++ sources/lesson-10/src/MyListTest/MyList.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MyList<E> {
     private int size;
-    private final int DEFAULT_CAPACITY = 10;
+    static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
 
     public MyList(int size, Object[] elements) {
@@ -14,10 +14,10 @@ public class MyList<E> {
 
     public MyList() {
         size = 0;
-        elements = new Object[10];
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
-    public int getSize() {
+    public int size() {
         return size;
     }
 
@@ -37,18 +37,12 @@ public class MyList<E> {
         this.elements = element;
     }
 
+    //Method in previous exercise
+
     public void ensureCapa() {
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
         size = newSize;
-    }
-
-    public void add(E e) {
-        if (size == elements.length) {
-            ensureCapa();
-        }
-        elements[size] = e;
-        size++;
     }
 
     public E get(int index) {
@@ -56,5 +50,72 @@ public class MyList<E> {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
         }
         return (E) elements[index];
+    }
+
+    public void printArray() {
+        for (int index = 0; index < size; index++) {
+            System.out.println(elements[index]);
+        }
+    }
+
+    //Method in the new exercise
+    public void add(int index, E element) {
+        E temp1 = (E) elements[index];
+        elements[index] = element;
+        for (int count = index + 1; count <= size; count++) {
+            E temp2 = (E) elements[count];
+            elements[count] = temp1;
+            temp1 = temp2;
+        }
+        size++;
+    }
+
+    public boolean add(E element) {
+        boolean check = false;
+        if (size == DEFAULT_CAPACITY) {
+            return false;
+        } else {
+            elements[size] = element;
+            size++;
+            check = true;
+        }
+        return check;
+    }
+
+    public E remove(int index) {
+        E element = (E) elements[index];
+        for (int count = index; count < size - 1; count++) {
+            elements[count] = elements[count + 1];
+        }
+        size--;
+        return element;
+    }
+
+//    @Override
+//    public Object clone() {
+//
+//    }
+
+    public boolean contains(E element) {
+        boolean check = false;
+        for (Object ele : elements) {
+            if ((E) ele == element) {
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void clear() {
+        for (Object element : elements) {
+            element = null;
+        }
+        elements = null;
     }
 }
--- sources/lesson-10/src/MyListTest/MyListTest.java
+++ sources/lesson-10/src/MyListTest/MyListTest.java
@@ -3,14 +3,19 @@ package MyListTest;
 public class MyListTest {
     public static void main(String[] args) {
         MyList<Integer> listInteger = new MyList<Integer>();
-        for (int element = 0; element < 5; element++) {
+        for (int element = 0; element < 8; element++) {
             listInteger.add(element);
         }
-
-        System.out.println(listInteger.get(0));
-        System.out.println(listInteger.get(1));
-        System.out.println(listInteger.get(2));
-        System.out.println(listInteger.get(3));
-        System.out.println(listInteger.get(-1));
+//        listInteger.add(2, 10);
+        listInteger.printArray();
+        System.out.println();
+        listInteger.remove(4);
+        System.out.println(listInteger.contains(7));
+        System.out.println(listInteger.contains(4));
+        listInteger.printArray();
+        listInteger.clear();
+        if (listInteger.getElement() == null) {
+            System.out.println("List is cleared!!");
+        }
     }
 }
