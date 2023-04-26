dae3416c32a0c35d2562f86ae866332c57b5c1b4
Reyon <chphungphat@gmail.com>
2023-04-26 08:57:35 +0700

    runnable

---
+++ sources/lesson-18/src/Testing.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Testing {
+    public static void main(String[] args) {
+        int[] x = new int[]{1, 2, 4};
+        System.out.print(Arrays.toString(x));
+    }
+}
