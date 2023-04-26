63e13073ef45eaf6d24c7f4a528d98de27b55312
Reyon <chphungphat@gmail.com>
2023-04-26 09:09:48 +0700

    Factory Animal

---
+++ sources/lesson-21/src/FactoryMethodTesting/Animal.java
@@ -0,0 +1,5 @@
+package FactoryMethodTesting;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ sources/lesson-21/src/FactoryMethodTesting/Dog.java
@@ -0,0 +1,10 @@
+package FactoryMethodTesting;
+
+public class Dog extends Animal {
+    @Override
+    public String makeSound() {
+        return "Woof";
+    }
+
+
+}
