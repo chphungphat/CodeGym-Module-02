29714fb2b1b1fb7a617f91b79f267f4a5a892796
Reyon <chphungphat@gmail.com>
2023-04-27 13:36:46 +0700

    task

---
+++ sources/lesson-21/src/TaskBuilder/MainTest.java
@@ -0,0 +1,11 @@
+package TaskBuilder;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Task task = new TaskBuilder(5)
+                .setDescription("Hello")
+                .setSummary("Test")
+                .build();
+        System.out.println(task.getDescription());
+    }
+}
